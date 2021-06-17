8c9126476cf1d6a7e4eff570bfa4dfc5b8a9efc8
JonathanRaiden1780 <mejahv_hs@hotmail.com>
2021-06-16 21:16:40 -0500

    inicio de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { ClientRegisterComponent } from './components/client-register/client-register.component';
+import { LoginComponent } from './components/login/login.component';
+import { TransferComponent } from './components/transfer/transfer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'accounts', component: AccountsComponent},
+  {path: 'register', component: ClientRegisterComponent},
+  {path: 'transfer', component: TransferComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ClientRegisterComponent } from './components/client-register/client-register.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ClientRegisterComponent,
+    AccountsComponent,
+    TransferComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
